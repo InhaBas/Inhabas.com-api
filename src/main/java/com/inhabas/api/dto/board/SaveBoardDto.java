61fd86d1c61cdbafaac2e07e033c9847e4e4d7da
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2022-01-17 22:56:37 +0900

    [Feature/#25] BoardDto, Service, Controller 생성 (#26)
    * SaveBoardDto, UpdateBoardDto 생성
    
    Authored-by: MingyeomKim <12201863@office.inha.ac.kr>

---
+++ src/main/java/com/inhabas/api/dto/board/SaveBoardDto.java
@@ -0,0 +1,19 @@
+package com.inhabas.api.dto.board;
+
+import com.inhabas.api.domain.board.Category;
+import com.inhabas.api.domain.member.Member;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class SaveBoardDto {
+    private String title;
+    private String contents;
+    private Category category;
+
+    public SaveBoardDto(String title, String contents, Category category) {
+        this.title = title;
+        this.contents = contents;
+        this.category = category;
+    }
+}
