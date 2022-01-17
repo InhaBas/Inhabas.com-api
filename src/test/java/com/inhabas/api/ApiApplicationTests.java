cfb321e44538eb454900bcb9ef36d168b738eb93
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-01-17 20:03:10 +0900

    [refactor] 통합테스트 시 in-memory db 사용하도록 설정.. 추후 testcontainer 도입 고려해야함.

--- src/test/java/com/inhabas/api/ApiApplicationTests.java
+++ src/test/java/com/inhabas/api/ApiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.inhabas.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.AUTO_CONFIGURED)
 class ApiApplicationTests {
 
 	@Test
