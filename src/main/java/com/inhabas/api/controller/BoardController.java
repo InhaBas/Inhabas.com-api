1134361f7aa859044f4d3d9f108bd7c5ba51b165
MingyeomKim <12201863@office.inha.ac.kr>
2022-01-17 23:01:28 +0900

    [refactor] SaveBoardDto 수정

--- src/main/java/com/inhabas/api/controller/BoardController.java
+++ src/main/java/com/inhabas/api/controller/BoardController.java
@@ -1,8 +1,10 @@
 package com.inhabas.api.controller;
 
+import com.inhabas.api.domain.board.NormalBoardRepository;
 import com.inhabas.api.dto.board.BoardDto;
 import com.inhabas.api.dto.board.SaveBoardDto;
 import com.inhabas.api.dto.board.UpdateBoardDto;
+
 import com.inhabas.api.service.board.BoardService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,6 +24,7 @@ import javax.persistence.EntityNotFoundException;
 public class BoardController {
 
     private final BoardService boardService;
+    private final NormalBoardRepository repository;
 
     @Operation(description = "게시글 조회")
     @GetMapping
