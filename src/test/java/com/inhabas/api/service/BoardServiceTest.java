d0b123bcb46678fbfd126906bd629fd2873ef237
MingyeomKim <12201863@office.inha.ac.kr>
2022-01-20 16:15:51 +0900

    [feature] BoardServiceTest 생성
    Mock 객체를 이용한 BoardServiceTest 코드 생성

--- src/test/java/com/inhabas/api/service/BoardServiceTest.java
+++ src/test/java/com/inhabas/api/service/BoardServiceTest.java
@@ -1,11 +1,31 @@
 package com.inhabas.api.service;
 
+import com.inhabas.api.domain.board.NormalBoardRepository;
+import com.inhabas.api.service.board.BoardService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.transaction.Transactional;
 
 @ExtendWith(MockitoExtension.class)
 public class BoardServiceTest {
+
+    BoardService boardService;
+    NormalBoardRepository boardRepository;
+
+    @BeforeEach
+    void setUp(){ // mock 객체 생성
+
+    }
+
+    @Test
+    public void TestName() {
+        //given
+
+        // when
+
+        // then
+    }
+
 }
