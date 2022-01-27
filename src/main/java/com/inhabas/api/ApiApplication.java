7d6539b5c0dd425f8454f5ba398e2a2c0057e767
Dong-Hyeon-Yu <81698076+Dong-Hyeon-Yu@users.noreply.github.com>
2022-01-27 12:19:06 +0900

    [feature/cicd] 슬랙 알림 및 배포 설정 (#42) (#43)
    * [feature/cicd] 슬랙 알림 및 배포 설정 (#42)
    
    * [feature] slack notification
    
    * [feature] spring cloud config 적용 
    
    * [feature] github action 배포파일 작성 (deploy-dev.yml)
    
    * [deprecate] 슬랙 알림 설정 안함.

--- src/main/java/com/inhabas/api/ApiApplication.java
+++ src/main/java/com/inhabas/api/ApiApplication.java
@@ -2,7 +2,6 @@ package com.inhabas.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class ApiApplication {
