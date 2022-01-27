4c10c278fbdbaa141e59ad47b487637243d019d4
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-01-27 13:52:04 +0900

    [config] 실행 시 pid 파일 생성하도록 설정

--- src/main/java/com/inhabas/api/ApiApplication.java
+++ src/main/java/com/inhabas/api/ApiApplication.java
@@ -2,12 +2,15 @@ package com.inhabas.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication app = new SpringApplication(ApiApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
 	}
 
 }
