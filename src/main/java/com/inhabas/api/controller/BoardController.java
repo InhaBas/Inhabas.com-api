90842b62b5ce9129f49846d3c83f32aba7357123
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2022-02-25 20:42:54 +0900

    [refactor] ResponseEntity 반환 값 수정

--- src/main/java/com/inhabas/api/controller/BoardController.java
+++ src/main/java/com/inhabas/api/controller/BoardController.java
@@ -4,6 +4,8 @@ import com.inhabas.api.dto.board.BoardDto;
 import com.inhabas.api.dto.board.SaveBoardDto;
 import com.inhabas.api.dto.board.UpdateBoardDto;
 
+import com.inhabas.api.security.argumentResolver.Authenticated;
+import com.inhabas.api.security.domain.AuthUserDetail;
 import com.inhabas.api.service.board.BoardService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -27,34 +31,34 @@ public class BoardController {
 
     @Operation(description = "게시글 조회")
     @GetMapping
-    public BoardDto getBoard(@RequestParam Integer id) {
-        return boardService.getBoard(id);
+    public ResponseEntity<BoardDto> getBoard(@RequestParam Integer id) {
+        return new ResponseEntity<>(boardService.getBoard(id), HttpStatus.OK);
     }
 
     @Operation(description = "모든 게시글 조회")
     @GetMapping("/all")
-    public Page<BoardDto> getBoardList(
+    public ResponseEntity<Page<BoardDto>> getBoardList(
             Pageable pageable,
-            @RequestParam Integer menuId
-    ) {
-        return boardService.getBoardList(menuId, pageable);
+            @RequestParam Integer menuId) {
+        return new ResponseEntity<>(boardService.getBoardList(menuId, pageable), HttpStatus.OK);
     }
 
     @Operation(description = "게시글 추가")
     @PostMapping
-    public Integer addBoard(@Valid @RequestBody SaveBoardDto saveBoardDto) {
-        return boardService.write(saveBoardDto);
+    public ResponseEntity<Integer> addBoard(@Authenticated Integer userId, @Valid @RequestBody SaveBoardDto saveBoardDto) {
+        return new ResponseEntity<>(boardService.write(userId, saveBoardDto), HttpStatus.CREATED);
     }
 
     @Operation(description = "게시글 수정")
     @PutMapping
-    public Integer updateBoard(@Valid @RequestBody UpdateBoardDto updateBoardDto) {
-        return boardService.update(updateBoardDto);
+    public ResponseEntity<Integer> updateBoard(@Authenticated Integer userId ,@Valid @RequestBody UpdateBoardDto updateBoardDto) {
+        return new ResponseEntity<>(boardService.update(userId, updateBoardDto), HttpStatus.OK);
     }
 
     @Operation(description = "게시글 삭제")
     @DeleteMapping
-    public void deleteBoard(@RequestParam Integer id) {
+    public ResponseEntity deleteBoard(@RequestParam Integer id) {
         boardService.delete(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
