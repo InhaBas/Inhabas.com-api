b1715231d63c0973c09d44db1aa19a6d602eb62c
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-03-22 17:38:04 +0900

    [invalid] MemberTeam EAGER 속성 없앰.

--- resource-server/src/main/java/com/inhabas/api/domain/member/MemberTeam.java
+++ resource-server/src/main/java/com/inhabas/api/domain/member/MemberTeam.java
@@ -18,11 +18,7 @@ public class MemberTeam {
     @JoinColumn(name="user_id", foreignKey = @ForeignKey(name = "fk_to_user"))
     private Member member;
 
-    /**
-     * 조인 엔티티를 참조하는 행위 자체가 team 을 얻기 위한 행동이기 때문에, <br>
-     * MemberTeam 을 가져오는 순간 team 까지 같이 가져온다.
-     */
-    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name="team_id", foreignKey = @ForeignKey(name = "fk_to_team"))
     private Team team;
 
