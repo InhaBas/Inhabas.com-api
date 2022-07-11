d03c1db005fd213d5330e73eab7f82aa73379739
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-07-11 21:32:25 +0900

    [feature] swagger 명세 적용

--- resource-server/src/main/java/com/inhabas/api/web/BudgetApplicationController.java
+++ resource-server/src/main/java/com/inhabas/api/web/BudgetApplicationController.java
@@ -8,6 +8,9 @@ import com.inhabas.api.domain.budget.dto.BudgetApplicationUpdateForm;
 import com.inhabas.api.domain.budget.usecase.BudgetApplicationService;
 import com.inhabas.api.domain.member.domain.valueObject.MemberId;
 import com.inhabas.api.web.argumentResolver.Authenticated;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -28,7 +31,12 @@ public class BudgetApplicationController {
 
     private final BudgetApplicationService budgetApplicationService;
 
+    @Operation(summary = "새로운 예산지원신청글을 추가한다.")
     @PostMapping
+    @ApiResponses({
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력"),
+    })
     public ResponseEntity<?> createApplication(
             @Authenticated MemberId loginMember, @Valid @RequestBody BudgetApplicationRegisterForm form) {
 
@@ -37,7 +45,13 @@ public class BudgetApplicationController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "예산지원신청글을 수정한다.")
     @PutMapping
+    @ApiResponses({
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력, 또는 게시글이 존재하지 않는 경우"),
+            @ApiResponse(responseCode = "401", description = "글쓴이가 아니면 수정 불가")
+    })
     public ResponseEntity<?> modifyApplication(
             @Authenticated MemberId loginMember, @Valid @RequestBody BudgetApplicationUpdateForm form) {
 
@@ -46,7 +60,13 @@ public class BudgetApplicationController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "예산지원신청글을 삭제한다.")
     @DeleteMapping("/{applicationId}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력, 또는 게시글이 존재하지 않는 경우"),
+            @ApiResponse(responseCode = "401", description = "글쓴이가 아니면 삭제 불가")
+    })
     public ResponseEntity<?> deleteApplication(
             @Authenticated MemberId loginMember, @PathVariable Integer applicationId) {
 
@@ -55,7 +75,9 @@ public class BudgetApplicationController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "예산지원신청글 내용을 조회한다.")
     @GetMapping("/{applicationId}")
+
     public ResponseEntity<BudgetApplicationDetailDto> getApplication(@PathVariable Integer applicationId) {
 
         BudgetApplicationDetailDto details = budgetApplicationService.getApplicationDetails(applicationId);
@@ -63,7 +85,9 @@ public class BudgetApplicationController {
         return ResponseEntity.ok(details);
     }
 
+    @Operation(summary = "예산지원 요청 게시글 목록을 조회한다.")
     @GetMapping("/search")
+    @ApiResponse(responseCode = "200", description = "검색 결과를 pagination 적용해서 반환, status 값 안주면 전체 상태를 검색함.")
     public ResponseEntity<Page<BudgetApplicationListDto>> getApplications(
             @Nullable @RequestParam ApplicationStatus status,
             @PageableDefault(size = 15, sort = "dateUsed", direction = Sort.Direction.DESC) Pageable pageable) {
