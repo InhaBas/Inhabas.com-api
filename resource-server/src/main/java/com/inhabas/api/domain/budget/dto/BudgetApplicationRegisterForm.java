43abf77bbe20ba3d72815c4e1f185c03d3fd620d
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-07-27 02:36:31 +0900

    [hotfix] applicationId를 파라미터로 넘겨주지 않았었음.

--- resource-server/src/main/java/com/inhabas/api/domain/budget/dto/BudgetApplicationRegisterForm.java
+++ resource-server/src/main/java/com/inhabas/api/domain/budget/dto/BudgetApplicationRegisterForm.java
@@ -48,6 +48,7 @@ public class BudgetApplicationRegisterForm {
                 .details(this.details)
                 .outcome(this.outcome)
                 .account(this.accounts)
+                .applicationWriter(applicationWriter)
                 .build();
     }
 }
