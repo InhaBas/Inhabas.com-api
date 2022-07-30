37473f90ed20d958c0c51f9ae7b3cded15d147be
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-07-30 17:59:08 +0900

    [feature] 로컬 개발용 ssh tunneling 설정

---
+++ resource-server/src/main/java/com/inhabas/api/config/sshTunneling/SshConfig.java
@@ -0,0 +1,64 @@
+package com.inhabas.api.config.sshTunneling;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import java.util.Properties;
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 로컬 개발용 ssh 터널링 설정. profile 이 local 일때만 유효
+ */
+//@Profile("local")
+@ConfigurationProperties(prefix = "ssh")
+@Setter @Getter
+public class SshConfig {
+
+    private String remoteHost;
+    private String user;
+    private String password;
+    private String privateKey;
+    private int externalPort;
+    private int internalPort;
+
+    private Session session;
+
+    public void closeSSH() {
+        session.disconnect();
+    }
+
+    public void init() {
+
+        Logger logger = LoggerFactory.getLogger(SshConfig.class);
+
+        try {
+            logger.info("{}, {}, {}, {}",remoteHost, user, password, privateKey);
+
+            logger.info("start ssh tunneling..");
+            JSch jSch = new JSch();
+
+            logger.info("creating ssh session");
+            jSch.addIdentity(privateKey);
+            session = jSch.getSession(user, remoteHost, externalPort);
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            logger.info("complete creating ssh session");
+
+            logger.info("start connecting ssh connection");
+            session.connect();
+            logger.info("success connecting ssh connection ");
+
+            logger.info("start forwarding");
+            session.setPortForwardingL(externalPort, remoteHost, internalPort);
+            logger.info("successfully connected to database");
+        } catch (Exception e){
+            logger.error("fail to make ssh tunneling");
+            e.printStackTrace();
+        }
+    }
+}
+
