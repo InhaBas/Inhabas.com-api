b734bfb582fd96d2cba39909e0075caaac53287a
whitemark <whtmdgus56@nate.com>
2023-09-05 00:25:20 +0900

    [refactor/#164] Dto 유효성 검증 어노테이션 추가

--- resource-server/src/main/java/com/inhabas/api/domain/member/dto/ContactDto.java
+++ resource-server/src/main/java/com/inhabas/api/domain/member/dto/ContactDto.java
@@ -3,14 +3,19 @@ package com.inhabas.api.domain.member.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 public class ContactDto {
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String phoneNumber;
 
+    @NotBlank
     private String email;
 
     public ContactDto(String name, String phoneNumber, String email) {
