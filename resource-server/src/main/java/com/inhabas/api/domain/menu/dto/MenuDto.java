49badc022fdf36603eec2c4399a75b143e506ae0
skytin1004 <skytin1004@sms7950>
2023-11-16 18:21:44 +0900

    swagger UI

--- resource-server/src/main/java/com/inhabas/api/domain/menu/dto/MenuDto.java
+++ resource-server/src/main/java/com/inhabas/api/domain/menu/dto/MenuDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.inhabas.api.domain.menu.domain.Menu;
 import com.inhabas.api.domain.menu.domain.valueObject.MenuId;
 import com.inhabas.api.domain.menu.domain.valueObject.MenuType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 /**
@@ -13,14 +14,16 @@ import lombok.Getter;
 public class MenuDto {
 
     @JsonUnwrapped
+
+    @Schema(description = "메뉴의 고유 식별자", example = "1")
     private MenuId id;
-
+    @Schema(description = "메뉴의 우선순위", example = "1")
     private Integer priority;
-
+    @Schema(description = "메뉴의 이름", example = "동아리 소개")
     private String name;
-
+    @Schema(description = "메뉴의 타입", example = "LIST")
     private String type;
-
+    @Schema(description = "메뉴의 설명", example = "동아리 소개 메뉴입니다.")
     private String description;
 
     public MenuDto(MenuId id, Integer priority, String name, MenuType type, String description) {
