339470f1d97a42f96733fa0ad869e101148da309
skytin1004 <skytin1004@gmail.com>
2024-01-19 08:44:31 +0900

    [refactor/Inhabas#165] 테스트 코드 리펙토링- domain.budget

--- resource-server/src/test/java/com/inhabas/api/domain/budget/usecase/BudgetApplicationProcessorTest.java
+++ resource-server/src/test/java/com/inhabas/api/domain/budget/usecase/BudgetApplicationProcessorTest.java
@@ -1,11 +1,15 @@
 package com.inhabas.api.domain.budget.usecase;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.inhabas.api.auth.domain.oauth2.member.domain.valueObject.StudentId;
 import com.inhabas.api.domain.budget.domain.BudgetHistory;
@@ -15,154 +19,158 @@ import com.inhabas.api.domain.budget.domain.valueObject.RejectReason;
 import com.inhabas.api.domain.budget.dto.BudgetApplicationStatusChangeRequest;
 import com.inhabas.api.domain.budget.repository.BudgetApplicationRepository;
 import com.inhabas.api.domain.budget.repository.BudgetHistoryRepository;
-import java.time.LocalDateTime;
-import java.util.Optional;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.assertj.core.api.Assertions;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(MockitoExtension.class)
 public class BudgetApplicationProcessorTest {
 
-    @InjectMocks
-    private BudgetApplicationProcessorImpl processor;
+    @InjectMocks private BudgetApplicationProcessorImpl processor;
 
-    @Mock
-    private BudgetApplicationRepository applicationRepository;
-    @Mock
-    private BudgetHistoryRepository historyRepository;
+    @Mock private BudgetApplicationRepository applicationRepository;
+    @Mock private BudgetHistoryRepository historyRepository;
 
     @DisplayName("대기중인 요청을 승인한다.")
     @Test
     public void waitingToApproveTest() {
-        //given
-        BudgetSupportApplication application = BudgetSupportApplication.builder()
-                .title("title")
-                .dateUsed(LocalDateTime.of(2020, 1, 1, 1, 1, 1))
-                .details("details")
-                .outcome(10000)
-                .applicationWriter(new StudentId("12171652"))
-                .account("기업 1234 홍길동")
-                .build();
+        // given
+        BudgetSupportApplication application =
+               BudgetSupportApplication.builder()
+                      .title("title")
+                      .dateUsed(LocalDateTime.of(2020, 1, 1, 1, 1, 1))
+                      .details("details")
+                      .outcome(10000)
+                      .applicationWriter(new StudentId("12171652"))
+                      .account("기업 1234 홍길동")
+                      .build();
         given(applicationRepository.findById(anyInt())).willReturn(Optional.of(application));
 
-        //when
+        // when
         BudgetApplicationStatusChangeRequest request =
-                new BudgetApplicationStatusChangeRequest("APPROVED", null);
+               new BudgetApplicationStatusChangeRequest("APPROVED", null);
         processor.process(1, request, new StudentId("18165249"));
 
-        //then
+        // then
         then(applicationRepository).should(times(1)).findById(anyInt());
-        ApplicationStatus status = (ApplicationStatus) ReflectionTestUtils.getField(application, "status");
-        assertThat(status).isEqualTo(ApplicationStatus.APPROVED);
+        ApplicationStatus status =
+               (ApplicationStatus) ReflectionTestUtils.getField(application, "status");
+        Assertions.assertThat(status).isEqualTo(ApplicationStatus.APPROVED);
     }
 
     @DisplayName("대기중인 요청을 거절한다.")
     @Test
     public void waitingToDenyTest() {
-        //given
-        BudgetSupportApplication application = BudgetSupportApplication.builder()
-                .title("title")
-                .dateUsed(LocalDateTime.of(2020, 1, 1, 1, 1, 1))
-                .details("details")
-                .outcome(10000)
-                .applicationWriter(new StudentId("12171652"))
-                .account("기업 1234 홍길동")
-                .build();
+        // given
+        BudgetSupportApplication application =
+               BudgetSupportApplication.builder()
+                      .title("title")
+                      .dateUsed(LocalDateTime.of(2020, 1, 1, 1, 1, 1))
+                      .details("details")
+                      .outcome(10000)
+                      .applicationWriter(new StudentId("12171652"))
+                      .account("기업 1234 홍길동")
+                      .build();
         given(applicationRepository.findById(anyInt())).willReturn(Optional.of(application));
 
-        //when
+        // when
         BudgetApplicationStatusChangeRequest request =
-                new BudgetApplicationStatusChangeRequest("DENIED", "중복 요청");
+               new BudgetApplicationStatusChangeRequest("DENIED", "중복 요청");
         processor.process(1, request, new StudentId("18165249"));
 
-        //then
+        // then
         then(applicationRepository).should(times(1)).findById(anyInt());
-        ApplicationStatus status = (ApplicationStatus) ReflectionTestUtils.getField(application, "status");
-        assertThat(status).isEqualTo(ApplicationStatus.DENIED);
+        ApplicationStatus status =
+               (ApplicationStatus) ReflectionTestUtils.getField(application, "status");
+        Assertions.assertThat(status).isEqualTo(ApplicationStatus.DENIED);
     }
 
     @DisplayName("대기중인 요청을 거절할 때, 거절 사유는 필수이다.")
     @Test
     public void rejectReasonIsNecessaryToDenyTest() {
-        //given
-        BudgetSupportApplication application = BudgetSupportApplication.builder()
-                .title("title")
-                .dateUsed(LocalDateTime.of(2020, 1, 1, 1, 1, 1))
-                .details("details")
-                .outcome(10000)
-                .applicationWriter(new StudentId("12171652"))
-                .account("기업 1234 홍길동")
-                .build();
+        // given
+        BudgetSupportApplication application =
+               BudgetSupportApplication.builder()
+                      .title("title")
+                      .dateUsed(LocalDateTime.of(2020, 1, 1, 1, 1, 1))
+                      .details("details")
+                      .outcome(10000)
+                      .applicationWriter(new StudentId("12171652"))
+                      .account("기업 1234 홍길동")
+                      .build();
         given(applicationRepository.findById(anyInt())).willReturn(Optional.of(application));
 
-        //when
+        // when
         BudgetApplicationStatusChangeRequest request =
-                new BudgetApplicationStatusChangeRequest("DENIED", null);
+               new BudgetApplicationStatusChangeRequest("DENIED", null);
 
-        //then
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> processor.process(1, request, new StudentId("18165249")));
+        // then
+        Assertions.assertThatThrownBy(() -> processor.process(1, request, new StudentId("18165249")))
+               .isInstanceOf(IllegalArgumentException.class)
+               .hasMessage(null);
         then(applicationRepository).should(times(1)).findById(anyInt());
     }
-
 
     @DisplayName("대기중인 요청을 완료처리한다.")
     @Test
     public void waitingToProcessTest() {
-        //given
-        BudgetSupportApplication application = BudgetSupportApplication.builder()
-                .title("title")
-                .dateUsed(LocalDateTime.of(2020, 1, 1, 1, 1, 1))
-                .details("details")
-                .outcome(10000)
-                .applicationWriter(new StudentId("12171652"))
-                .account("기업 1234 홍길동")
-                .build();
+        // given
+        BudgetSupportApplication application =
+               BudgetSupportApplication.builder()
+                      .title("title")
+                      .dateUsed(LocalDateTime.of(2020, 1, 1, 1, 1, 1))
+                      .details("details")
+                      .outcome(10000)
+                      .applicationWriter(new StudentId("12171652"))
+                      .account("기업 1234 홍길동")
+                      .build();
         given(applicationRepository.findById(anyInt())).willReturn(Optional.of(application));
         given(historyRepository.save(any(BudgetHistory.class))).willReturn(null);
 
-        //when
+        // when
         BudgetApplicationStatusChangeRequest request =
-                new BudgetApplicationStatusChangeRequest("PROCESSED", null);
+               new BudgetApplicationStatusChangeRequest("PROCESSED", null);
         processor.process(1, request, new StudentId("18165249"));
 
-        //then
+        // then
         then(applicationRepository).should(times(1)).findById(anyInt());
         then(historyRepository).should(times(1)).save(any(BudgetHistory.class));
-        ApplicationStatus status = (ApplicationStatus) ReflectionTestUtils.getField(application, "status");
-        assertThat(status).isEqualTo(ApplicationStatus.PROCESSED);
+        ApplicationStatus status =
+               (ApplicationStatus) ReflectionTestUtils.getField(application, "status");
+        Assertions.assertThat(status).isEqualTo(ApplicationStatus.PROCESSED);
     }
 
     @DisplayName("거절했던 신청이 수정된 뒤에 승인한다.")
     @Test
     public void deniedToApprovedTest() {
-        //given
-        BudgetSupportApplication application = BudgetSupportApplication.builder()
-                .title("title")
-                .dateUsed(LocalDateTime.of(2020, 1, 1, 1, 1, 1))
-                .details("details")
-                .outcome(10000)
-                .applicationWriter(new StudentId("12171652"))
-                .account("기업 1234 홍길동")
-                .build();
+        // given
+        BudgetSupportApplication application =
+               BudgetSupportApplication.builder()
+                      .title("title")
+                      .dateUsed(LocalDateTime.of(2020, 1, 1, 1, 1, 1))
+                      .details("details")
+                      .outcome(10000)
+                      .applicationWriter(new StudentId("12171652"))
+                      .account("기업 1234 홍길동")
+                      .build();
         ReflectionTestUtils.setField(application, "status", ApplicationStatus.DENIED);
         ReflectionTestUtils.setField(application, "rejectReason", new RejectReason("계좌번호 미기재"));
         given(applicationRepository.findById(anyInt())).willReturn(Optional.of(application));
 
-        //when
+        // when
         BudgetApplicationStatusChangeRequest request =
-                new BudgetApplicationStatusChangeRequest("APPROVED", null);
+               new BudgetApplicationStatusChangeRequest("APPROVED", null);
         processor.process(1, request, new StudentId("18165249"));
 
-        //then
+        // then
         then(applicationRepository).should(times(1)).findById(anyInt());
-        ApplicationStatus status = (ApplicationStatus) ReflectionTestUtils.getField(application, "status");
-        assertThat(status).isEqualTo(ApplicationStatus.APPROVED);
+        ApplicationStatus status =
+               (ApplicationStatus) ReflectionTestUtils.getField(application, "status");
+        Assertions.assertThat(status).isEqualTo(ApplicationStatus.APPROVED);
     }
 }
--- resource-server/src/test/java/com/inhabas/api/domain/budget/usecase/BudgetHistoryServiceTest.java
+++ resource-server/src/test/java/com/inhabas/api/domain/budget/usecase/BudgetHistoryServiceTest.java
@@ -1,84 +1,91 @@
 package com.inhabas.api.domain.budget.usecase;
 
-import com.inhabas.api.auth.domain.oauth2.member.domain.valueObject.StudentId;
-import com.inhabas.api.domain.budget.HistoryCannotModifiableException;
-import com.inhabas.api.domain.budget.BudgetHistoryNotFoundException;
-import com.inhabas.api.domain.budget.domain.BudgetHistory;
-import com.inhabas.api.domain.budget.dto.BudgetHistoryCreateForm;
-import com.inhabas.api.domain.budget.dto.BudgetHistoryDetailDto;
-import com.inhabas.api.domain.budget.dto.BudgetHistoryModifyForm;
-import com.inhabas.api.domain.budget.repository.BudgetHistoryRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.test.util.ReflectionTestUtils;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.inhabas.api.auth.domain.oauth2.member.domain.valueObject.StudentId;
+import com.inhabas.api.domain.budget.BudgetHistoryNotFoundException;
+import com.inhabas.api.domain.budget.HistoryCannotModifiableException;
+import com.inhabas.api.domain.budget.domain.BudgetHistory;
+import com.inhabas.api.domain.budget.dto.BudgetHistoryCreateForm;
+import com.inhabas.api.domain.budget.dto.BudgetHistoryDetailDto;
+import com.inhabas.api.domain.budget.dto.BudgetHistoryModifyForm;
+import com.inhabas.api.domain.budget.repository.BudgetHistoryRepository;
+import org.assertj.core.api.Assertions;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 @ExtendWith(MockitoExtension.class)
 public class BudgetHistoryServiceTest {
 
-    @InjectMocks
-    private BudgetHistoryServiceImpl budgetHistoryService;
+    @InjectMocks private BudgetHistoryServiceImpl budgetHistoryService;
 
-    @Mock
-    private BudgetHistoryRepository repository;
+    @Mock private BudgetHistoryRepository repository;
 
     @DisplayName("총무가 회계내역을 추가한다.")
     @Test
     public void createBudgetHistoryTest() {
 
-        //given
-        BudgetHistoryCreateForm form = new BudgetHistoryCreateForm(
-                LocalDateTime.of(2000, 1, 1, 1, 1, 1),
-                "서버운영비", "aws 작년 서버비용", "12345678", 0, 500000);
+        // given
+        BudgetHistoryCreateForm form =
+               new BudgetHistoryCreateForm(
+                      LocalDateTime.of(2000, 1, 1, 1, 1, 1), "서버운영비", "aws 작년 서버비용", "12345678", 0, 500000);
         StudentId cfo = new StudentId("12171652");
         given(repository.save(any(BudgetHistory.class))).willReturn(null);
 
-        //when
+        // when
         budgetHistoryService.createNewHistory(form, cfo);
 
-        //then
+        // then
         then(repository).should(times(1)).save(any(BudgetHistory.class));
     }
 
     @DisplayName("총무가 회계내역을 수정한다.")
     @Test
     public void modifyBudgetHistoryTest() {
-        //when
+        // when
         StudentId CFO = new StudentId("12171652");
-        BudgetHistory history = BudgetHistory.builder()
-                .title("서버 운영비")
-                .details("작년 aws 운영 비용")
-                .income(0)
-                .outcome(500000)
-                .dateUsed(LocalDateTime.of(2000, 1, 1, 1, 1, 1))
-                .personReceived(new StudentId("10982942"))
-                .personInCharge(CFO)
-                .build();
+        BudgetHistory history =
+               BudgetHistory.builder()
+                      .title("서버 운영비")
+                      .details("작년 aws 운영 비용")
+                      .income(0)
+                      .outcome(500000)
+                      .dateUsed(LocalDateTime.of(2000, 1, 1, 1, 1, 1))
+                      .personReceived(new StudentId("10982942"))
+                      .personInCharge(CFO)
+                      .build();
         ReflectionTestUtils.setField(history, "id", 1);
         given(repository.findById(any())).willReturn(Optional.of(history));
 
-        //when
-        BudgetHistoryModifyForm form = new BudgetHistoryModifyForm(
-                LocalDateTime.of(2000, 1, 1, 1, 1, 1),
-                "서버운영비", "aws 작년 서버비용", "12345678", 0, 500000, 1);
+        // when
+        BudgetHistoryModifyForm form =
+               new BudgetHistoryModifyForm(
+                      LocalDateTime.of(2000, 1, 1, 1, 1, 1),
+                      "서버운영비",
+                      "aws 작년 서버비용",
+                      "12345678",
+                      0,
+                      500000,
+                      1);
         budgetHistoryService.modifyHistory(form, CFO);
 
-        //then
+        // then
         then(repository).should(times(1)).findById(any());
         then(repository).should(times(1)).save(any(BudgetHistory.class));
     }
@@ -86,65 +93,84 @@ public class BudgetHistoryServiceTest {
     @DisplayName("총무는 이전의 다른 총무가 작성한 내역을 수정할 수 없다.")
     @Test
     public void cannotModifyBudgetHistoryOfOtherCFO() {
-        //given
+        // given
         StudentId previousCFO = new StudentId("12171652");
         StudentId currentCFO = new StudentId("99999999");
-        BudgetHistory history = BudgetHistory.builder()
-                .title("서버 운영비")
-                .details("작년 aws 운영 비용")
-                .income(0)
-                .outcome(500000)
-                .dateUsed(LocalDateTime.of(2000, 1, 1, 1, 1, 1))
-                .personReceived(new StudentId("10982942"))
-                .personInCharge(previousCFO)
-                .build();
+        BudgetHistory history =
+               BudgetHistory.builder()
+                      .title("서버 운영비")
+                      .details("작년 aws 운영 비용")
+                      .income(0)
+                      .outcome(500000)
+                      .dateUsed(LocalDateTime.of(2000, 1, 1, 1, 1, 1))
+                      .personReceived(new StudentId("10982942"))
+                      .personInCharge(previousCFO)
+                      .build();
         ReflectionTestUtils.setField(history, "id", 1);
         given(repository.findById(any())).willReturn(Optional.of(history));
 
-        //when
-        BudgetHistoryModifyForm form = new BudgetHistoryModifyForm(
-                LocalDateTime.of(2000, 1, 1, 1, 1, 1),
-                "서버운영비", "aws 작년 서버비용", "12345678", 0, 500000, 1);
-        Assertions.assertThrows(AccessDeniedException.class,
-                () -> budgetHistoryService.modifyHistory(form, currentCFO));
+        // when
+        BudgetHistoryModifyForm form =
+               new BudgetHistoryModifyForm(
+                      LocalDateTime.of(2000, 1, 1, 1, 1, 1),
+                      "서버운영비",
+                      "aws 작년 서버비용",
+                      "12345678",
+                      0,
+                      500000,
+                      1);
+
+        Assertions.assertThatThrownBy(() -> budgetHistoryService.modifyHistory(form, currentCFO))
+               .isInstanceOf(AccessDeniedException.class)
+               .hasMessage("다른 담당자가 작성한 내역은 수정할 수 없습니다!");
     }
 
     @DisplayName("수정 시도할 때 히스토리 내역이 존재하지 않으면 오류를 던진다.")
     @Test
     public void raiseNotFoundExceptionWhenModifyingTest() {
-        //given
+        // given
         given(repository.findById(any())).willReturn(Optional.empty());
 
-        //when
-        BudgetHistoryModifyForm form = new BudgetHistoryModifyForm(
-                LocalDateTime.of(2000, 1, 1, 1, 1, 1),
-                "서버운영비", "aws 작년 서버비용", "12345678", 0, 500000, 1);
-        Assertions.assertThrows(BudgetHistoryNotFoundException.class,
-                () -> budgetHistoryService.modifyHistory(form, new StudentId("12171652")));
+        // when
+        BudgetHistoryModifyForm form =
+               new BudgetHistoryModifyForm(
+                      LocalDateTime.of(2000, 1, 1, 1, 1, 1),
+                      "서버운영비",
+                      "aws 작년 서버비용",
+                      "12345678",
+                      0,
+                      500000,
+                      1);
+
+        Assertions.assertThatThrownBy(
+                      () -> budgetHistoryService.modifyHistory(form, new StudentId("12171652")))
+               .isInstanceOf(BudgetHistoryNotFoundException.class)
+               .hasMessage("cannot find such budget history!");
     }
 
     @DisplayName("총무가 회계 내역을 삭제한다.")
     @Test
     public void deleteBudgetHistoryTest() {
-        //given
+        // given
         StudentId CFO = new StudentId("12171652");
         Integer historyId = 1;
-        BudgetHistory history = BudgetHistory.builder()
-                .title("서버 운영비")
-                .details("작년 aws 운영 비용")
-                .income(0)
-                .outcome(500000)
-                .dateUsed(LocalDateTime.of(2000, 1, 1, 1, 1, 1))
-                .personReceived(new StudentId("10982942"))
-                .personInCharge(CFO)
-                .build();
+        BudgetHistory history =
+               BudgetHistory.builder()
+                      .title("서버 운영비")
+                      .details("작년 aws 운영 비용")
+                      .income(0)
+                      .outcome(500000)
+                      .dateUsed(LocalDateTime.of(2000, 1, 1, 1, 1, 1))
+                      .personReceived(new StudentId("10982942"))
+                      .personInCharge(CFO)
+                      .build();
         ReflectionTestUtils.setField(history, "id", historyId);
         given(repository.findById(any())).willReturn(Optional.of(history));
 
-        //when
+        // when
         budgetHistoryService.deleteHistory(historyId, CFO);
 
-        //then
+        // then
         then(repository).should(times(1)).findById(anyInt());
         then(repository).should(times(1)).deleteById(anyInt());
     }
@@ -152,83 +178,88 @@ public class BudgetHistoryServiceTest {
     @DisplayName("이전의 다른 담당자가 작성했던 기록은 삭제할 수 없다.")
     @Test
     public void cannotDeleteBudgetHistoryTest() {
-        //given
+        // given
         StudentId previousCFO = new StudentId("12171652");
         StudentId currentCFO = new StudentId("99999999");
         Integer historyId = 1;
-        BudgetHistory history = BudgetHistory.builder()
-                .title("서버 운영비")
-                .details("작년 aws 운영 비용")
-                .income(0)
-                .outcome(500000)
-                .dateUsed(LocalDateTime.of(2000, 1, 1, 1, 1, 1))
-                .personReceived(new StudentId("10982942"))
-                .personInCharge(previousCFO)
-                .build();
+        BudgetHistory history =
+               BudgetHistory.builder()
+                      .title("서버 운영비")
+                      .details("작년 aws 운영 비용")
+                      .income(0)
+                      .outcome(500000)
+                      .dateUsed(LocalDateTime.of(2000, 1, 1, 1, 1, 1))
+                      .personReceived(new StudentId("10982942"))
+                      .personInCharge(previousCFO)
+                      .build();
         ReflectionTestUtils.setField(history, "id", historyId);
         given(repository.findById(any())).willReturn(Optional.of(history));
 
-        //when
-        Assertions.assertThrows(HistoryCannotModifiableException.class,
-                () -> budgetHistoryService.deleteHistory(historyId, currentCFO));
+        // when
+        Assertions.assertThatThrownBy(() -> budgetHistoryService.deleteHistory(historyId, currentCFO))
+               .isInstanceOf(HistoryCannotModifiableException.class)
+               .hasMessage("다른 담당자가 작성한 내역은 수정할 수 없습니다!");
 
-        //then
+        // then
         then(repository).should(times(1)).findById(anyInt());
     }
 
     @DisplayName("존재하지 않는 기록을 삭제하려고 하면 예외를 던진다.")
     @Test
     public void raiseNotFoundExceptionWhenDeletingTest() {
-        //given
+        // given
         given(repository.findById(any())).willReturn(Optional.empty());
 
-        //when
-        Assertions.assertThrows(BudgetHistoryNotFoundException.class,
-                () -> budgetHistoryService.deleteHistory(1, new StudentId("12171652")));
+        // when
+        Assertions.assertThatThrownBy(
+                      () -> budgetHistoryService.deleteHistory(1, new StudentId("12171652")))
+               .isInstanceOf(BudgetHistoryNotFoundException.class)
+               .hasMessage("cannot find such budget history!");
 
-        //then
+        // then
         then(repository).should(times(1)).findById(anyInt());
     }
 
     @DisplayName("회계내역을 한 페이지를 출력한다.")
     @Test
     public void getListOfBudgetHistoryTest() {
-        //given
+        // given
         given(repository.search(any(), any())).willReturn(null);
 
-        //when
+        // when
         budgetHistoryService.searchHistoryList(null, Pageable.ofSize(15));
 
-        //then
+        // then
         then(repository).should(times(1)).search(any(), any());
     }
 
     @DisplayName("회계내역을 id 로 조회한다.")
     @Test
     public void getOneBudgetHistoryTest() {
-        //given
+        // given
         given(repository.findDtoById(anyInt()))
-                .willReturn(Optional.of(new BudgetHistoryDetailDto(
-                        null, null, null, null, null, null,
-                        null, null, null, null,
-                        null, null, null)));
+               .willReturn(
+                      Optional.of(
+                             new BudgetHistoryDetailDto(
+                                    null, null, null, null, null, null, null, null, null, null, null, null, null)));
 
-        //when
+        // when
         budgetHistoryService.getHistory(2);
 
-        //then
+        // then
         then(repository).should(times(1)).findDtoById(anyInt());
     }
 
     @DisplayName("id 에 해당하는 회계내역이 없는 경우, NotFoundException을 던진다.")
     @Test
     public void cannotFindBudgetHistoryFindById() {
-        //given
+        // given
         given(repository.findDtoById(anyInt())).willReturn(Optional.empty());
 
-        //when
-        Assertions.assertThrows(BudgetHistoryNotFoundException.class,
-                () -> budgetHistoryService.getHistory(2));
+        // when
+        Assertions.assertThatThrownBy(() -> budgetHistoryService.getHistory(2))
+               .isInstanceOf(BudgetHistoryNotFoundException.class)
+               .hasMessage("cannot find such budget history!");
 
         then(repository).should(times(1)).findDtoById(anyInt());
     }
@@ -236,13 +267,13 @@ public class BudgetHistoryServiceTest {
     @DisplayName("기록이 존재하는 년도를 모두 가져온다.")
     @Test
     public void fetchAllYearOfHistoryTest() {
-        //given
+        // given
         given(repository.findAllYear()).willReturn(null);
 
-        //when
+        // when
         budgetHistoryService.getAllYearOfHistory();
 
-        //then
+        // then
         then(repository).should(times(1)).findAllYear();
     }
 }
