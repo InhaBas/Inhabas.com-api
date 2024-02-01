521a307d4117346b409a86f54f39020707016613
whitemark <whtmdgus56@nate.com>
2024-02-01 23:01:32 +0900

    [feature/#225] AWS SES 설정

---
+++ module-auth/src/main/java/com/inhabas/api/auth/config/AwsSESConfig.java
@@ -0,0 +1,35 @@
+package com.inhabas.api.auth.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsSESConfig {
+
+    @Value("${cloud.aws.ses.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.ses.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService() {
+
+        final BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        final AWSStaticCredentialsProvider awsStaticCredentialsProvider =
+                new AWSStaticCredentialsProvider(basicAWSCredentials);
+
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withCredentials(awsStaticCredentialsProvider)
+                .withRegion(region)
+                .build();
+    }
+}
---
+++ module-auth/src/main/java/com/inhabas/api/auth/config/TemplateConfig.java
@@ -0,0 +1,32 @@
+package com.inhabas.api.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class TemplateConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver springResourceTemplateResolver() {
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setPrefix("classpath:/");
+        springResourceTemplateResolver.setCharacterEncoding("UTF-8");
+        springResourceTemplateResolver.setSuffix(".html");
+        springResourceTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        springResourceTemplateResolver.setCacheable(false);
+
+        return springResourceTemplateResolver;
+    }
+
+    @Bean
+    public TemplateEngine htmlTemplateEngine(SpringResourceTemplateResolver springResourceTemplateResolver) {
+        TemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(springResourceTemplateResolver);
+
+        return templateEngine;
+    }
+}
