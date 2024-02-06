83a7e95f774d5f4cf7a08da3cb2edd447cf16640
skytin1004 <skytin1004@gmail.com>
2024-02-06 11:32:53 +0900

    [feature/Inhabas#228] 공모전 게시판 DTO D-Day 추가

--- resource-server/src/main/java/com/inhabas/api/domain/contest/dto/ContestBoardDto.java
+++ resource-server/src/main/java/com/inhabas/api/domain/contest/dto/ContestBoardDto.java
@@ -1,12 +1,14 @@
 package com.inhabas.api.domain.contest.dto;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.inhabas.api.domain.file.dto.FileDownloadDto;
 
 // 공모전 게시판 전체 조회
@@ -25,6 +27,9 @@ public class ContestBoardDto {
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
   private LocalDate dateContestEnd;
 
+  @JsonProperty("D-day")
+  private long dDay;
+
   private FileDownloadDto thumbnail;
 
   @Builder
@@ -42,6 +47,7 @@ public class ContestBoardDto {
     this.association = association;
     this.dateContestStart = dateContestStart;
     this.dateContestEnd = dateContestEnd;
+    this.dDay = ChronoUnit.DAYS.between(LocalDate.now(), dateContestEnd);
     this.thumbnail = thumbnail;
   }
 }
