1ee68cade2f74aa669d32dd82d9e58824072f32d
skytin1004 <skytin1004@gmail.com>
2024-02-16 08:49:19 +0900

    [feature/Inhabas#228] spotlessApply

--- resource-server/src/main/java/com/inhabas/api/domain/contest/domain/ContestField.java
+++ resource-server/src/main/java/com/inhabas/api/domain/contest/domain/ContestField.java
@@ -1,9 +1,6 @@
 package com.inhabas.api.domain.contest.domain;
 
-import com.inhabas.api.domain.BaseEntity;
-import com.inhabas.api.domain.contest.domain.valueObject.ContestFieldName;
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
@@ -11,10 +8,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.inhabas.api.domain.BaseEntity;
+import com.inhabas.api.domain.contest.domain.valueObject.ContestFieldName;
 
 @Entity
 @Table(name = "CONTEST_FIELD")
