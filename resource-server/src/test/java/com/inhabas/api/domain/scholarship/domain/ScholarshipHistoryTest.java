733c2cfe57aa4a04a71e874cda868ceca91bac64
whitemark <whtmdgus56@nate.com>
2024-03-15 13:52:30 +0900

    [feature/#251] scholarshipHistory entity test

---
+++ resource-server/src/test/java/com/inhabas/api/domain/scholarship/domain/ScholarshipHistoryTest.java
@@ -0,0 +1,41 @@
+package com.inhabas.api.domain.scholarship.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.inhabas.api.auth.domain.oauth2.member.domain.entity.Member;
+import com.inhabas.api.domain.board.domain.valueObject.Title;
+import com.inhabas.api.domain.member.domain.entity.MemberTest;
+import com.inhabas.api.domain.scholarship.dto.SaveScholarshipHistoryDto;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ScholarshipHistoryTest {
+
+  @DisplayName("Dto 를 사용해서 scholarshipHistory를 수정한다.")
+  @Test
+  void update() {
+    // given
+    Member writer = MemberTest.chiefMember();
+    ScholarshipHistory scholarshipHistory =
+        ScholarshipHistory.builder()
+            .writer(writer)
+            .title(new Title("oldTitle"))
+            .dateHistory(LocalDateTime.now())
+            .build();
+
+    SaveScholarshipHistoryDto saveScholarshipHistoryDto =
+        SaveScholarshipHistoryDto.builder().title("title").dateHistory(LocalDateTime.now()).build();
+
+    // when
+    scholarshipHistory.update(writer, saveScholarshipHistoryDto);
+
+    // then
+    assertThat(scholarshipHistory)
+        .extracting(
+            ScholarshipHistory -> scholarshipHistory.getTitle().getValue(),
+            ScholarshipHistory -> scholarshipHistory.getDateHistory())
+        .containsExactly(
+            saveScholarshipHistoryDto.getTitle(), saveScholarshipHistoryDto.getDateHistory());
+  }
+}
