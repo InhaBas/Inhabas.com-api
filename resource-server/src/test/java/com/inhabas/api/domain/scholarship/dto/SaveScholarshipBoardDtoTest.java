6a2820a993fbaa7a9ac8ca537cab681876edfb9b
whitemark <whtmdgus56@nate.com>
2024-03-21 18:07:45 +0900

    [feature/#251] scholarship entity, dto test

---
+++ resource-server/src/test/java/com/inhabas/api/domain/scholarship/dto/SaveScholarshipBoardDtoTest.java
@@ -0,0 +1,62 @@
+package com.inhabas.api.domain.scholarship.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SaveScholarshipBoardDtoTest {
+  private static ValidatorFactory validatorFactory;
+  private static Validator validator;
+
+  @BeforeAll
+  public static void init() {
+    validatorFactory = Validation.buildDefaultValidatorFactory();
+    validator = validatorFactory.getValidator();
+  }
+
+  @AfterAll
+  public static void close() {
+    validatorFactory.close();
+  }
+
+  @DisplayName("SaveScholarshipBoardDto 객체를 정상적으로 생성한다.")
+  @Test
+  public void SaveProjectBoardDto_is_OK() {
+    // given
+    SaveScholarshipBoardDto saveScholarshipBoardDto =
+        new SaveScholarshipBoardDto("title", "content", LocalDateTime.now(), null);
+
+    // when
+    Set<ConstraintViolation<SaveScholarshipBoardDto>> violations =
+        validator.validate(saveScholarshipBoardDto);
+
+    // then
+    assertThat(violations).isEmpty();
+  }
+
+  @DisplayName("SaveScholarshipBoardDto title, content, dateHistory 필드가 null 이면 validation 실패")
+  @Test
+  public void Content_is_null() {
+    // given
+    SaveScholarshipBoardDto saveScholarshipBoardDto =
+        new SaveScholarshipBoardDto(null, null, null, null);
+
+    // when
+    Set<ConstraintViolation<SaveScholarshipBoardDto>> violations =
+        validator.validate(saveScholarshipBoardDto);
+
+    // then
+    assertThat(violations).hasSize(3);
+  }
+}
---
+++ resource-server/src/test/java/com/inhabas/api/domain/scholarship/dto/ScholarshipBoardDtoTest.java
@@ -0,0 +1,72 @@
+package com.inhabas.api.domain.scholarship.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.inhabas.api.auth.domain.oauth2.member.domain.entity.Member;
+import com.inhabas.api.domain.member.domain.entity.MemberTest;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ScholarshipBoardDtoTest {
+
+  private static ValidatorFactory validatorFactory;
+  private static Validator validator;
+
+  @BeforeAll
+  public static void init() {
+    validatorFactory = Validation.buildDefaultValidatorFactory();
+    validator = validatorFactory.getValidator();
+  }
+
+  @AfterAll
+  public static void close() {
+    validatorFactory.close();
+  }
+
+  @DisplayName("ScholarshipBoardDto 객체를 정상적으로 생성한다.")
+  @Test
+  public void ScholarshipBoardDto_is_OK() {
+    // given
+    Member writer = MemberTest.chiefMember();
+    ReflectionTestUtils.setField(writer, "id", 1L);
+    ScholarshipBoardDto scholarshipBoardDto =
+        new ScholarshipBoardDto(1L, "title", writer, LocalDateTime.now(), LocalDateTime.now());
+
+    // when
+    Set<ConstraintViolation<ScholarshipBoardDto>> violations =
+        validator.validate(scholarshipBoardDto);
+
+    // then
+    assertThat(violations).isEmpty();
+  }
+
+  @DisplayName("ScholarshipBoardDto 각각의 필드가 null 이면 validation 실패")
+  @Test
+  public void Content_is_null() {
+    // given
+    Member writer = MemberTest.chiefMember();
+    ReflectionTestUtils.setField(writer, "id", 1L);
+    ScholarshipBoardDto scholarshipBoardDto =
+        new ScholarshipBoardDto(null, null, writer, null, null);
+
+    // when
+    Set<ConstraintViolation<ScholarshipBoardDto>> violations =
+        validator.validate(scholarshipBoardDto);
+
+    // then
+    assertThat(violations).hasSize(4);
+  }
+}
