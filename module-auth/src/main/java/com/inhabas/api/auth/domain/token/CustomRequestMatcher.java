621fbb7dcc1a235ca64eeb672e09e07119b432a2
agree0002 <donghui1365@outlook.kr>
2026-07-06 21:50:18 +0900

    [refactor/#386] AntPathRequestMatcher -> PathPatternRequestMatcher

--- module-auth/src/main/java/com/inhabas/api/auth/domain/token/CustomRequestMatcher.java
+++ module-auth/src/main/java/com/inhabas/api/auth/domain/token/CustomRequestMatcher.java
@@ -2,25 +2,22 @@ package com.inhabas.api.auth.domain.token;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /** 기존 matches와 달리 request와 일치하지 않으면 True 반환 */
 public class CustomRequestMatcher implements RequestMatcher {
 
-  private final AntPathRequestMatcher antPathRequestMatcher;
-  private final String httpMethod;
+  private final RequestMatcher requestMatcher;
 
   public CustomRequestMatcher(String path, String httpMethod) {
-
-    this.httpMethod = httpMethod;
-    this.antPathRequestMatcher = new AntPathRequestMatcher(path, httpMethod);
+    this.requestMatcher =
+        PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.valueOf(httpMethod), path);
   }
 
   @Override
   public boolean matches(HttpServletRequest request) {
-
-    String requestMethod = request.getMethod();
-    return !(httpMethod.equalsIgnoreCase(requestMethod) && antPathRequestMatcher.matches(request));
+    return !requestMatcher.matches(request);
   }
 }
